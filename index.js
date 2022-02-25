3b7ecfc417285fd99f3f2c75ddd463ce049fc204
Pere_ebi Egbekun <pereebiegbekun@gmail.com>
2022-02-25 23:21:20 +0100

    comitting task one

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose.Schema;
+
+mongoose.model(
+  'Configuration',
+  new Schema({
+    key: String,
+    value: Schema.Types.Mixed,
+    type: String,
+  }),
+);
